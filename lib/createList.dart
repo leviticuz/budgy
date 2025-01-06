c3014153cbb920dbaa6ef712ab85a25194c79510
Zhaira <zhairajeanebryne.cielo@cvsu.edu.ph>
2025-01-06 11:31:38 +0800

    medj pinaka updated

--- lib/createList.dart
+++ lib/createList.dart
@@ -17,7 +17,7 @@ class CreateTab extends StatelessWidget {
     required this.selectedDate,
     required this.onDatePicked,
     required this.onSelectDate,
-    required this.isNewList
+    required this.isNewList,
   });
 
   @override
@@ -26,167 +26,174 @@ class CreateTab extends StatelessWidget {
       titleController.clear();
       budgetController.clear();
     }
+
     return Scaffold(
       backgroundColor: Color(0xFFB1E8DE),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Spacer(),
-            Container(
-              padding: EdgeInsets.all(19.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 19),
-                    padding: EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 6,
-                          offset: Offset(0, 4),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(19.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  children: [
+                    // Title Input Field
+                    Container(
+                      margin: EdgeInsets.only(bottom: 19),
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 6,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: titleController,
+                        decoration: InputDecoration(
+                          labelText: 'Title',
+                          border: InputBorder.none,
                         ),
-                      ],
-                    ),
-                    child: TextField(
-                      controller: titleController,
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                        border: InputBorder.none,
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 19),
-                    padding: EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 6,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
+
+                    // Date Input Field
+                    Container(
+                      margin: EdgeInsets.only(bottom: 19),
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 6,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: dateController,
+                              decoration: InputDecoration(
+                                labelText: 'Pick Date',
+                                border: InputBorder.none,
+                              ),
+                              readOnly: true,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.calendar_today),
+                            onPressed: onSelectDate,
+                          ),
+                        ],
+                      ),
                     ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            controller: dateController,
+
+                    // Budget Input Field
+                    Container(
+                      margin: EdgeInsets.only(bottom: 19),
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 6,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextField(
+                            controller: budgetController,
                             decoration: InputDecoration(
-                              labelText: 'Pick Date',
+                              labelText: 'Budget (₱)',
                               border: InputBorder.none,
                             ),
-                            readOnly: true,
-                          ),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.calendar_today),
-                          onPressed: onSelectDate,
-                        ),
-                      ],
-                    ),
-                  ), Container(
-                    margin: EdgeInsets.only(bottom: 19),
-                    padding: EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 6,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextField(
-                          controller: budgetController,
-                          decoration: InputDecoration(
-                            labelText: 'Budget (₱)',
-                            border: InputBorder.none,
-                          ),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                            LengthLimitingTextInputFormatter(7),
-                          ],
-                          onChanged: (value) {
-                            String newValue = value.replaceAll(',', '');
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(7),
+                            ],
+                            onChanged: (value) {
+                              String newValue = value.replaceAll(',', '');
 
-                            if (newValue.isNotEmpty &&
-                                int.tryParse(newValue) != null) {
-                              int parsedValue = int.parse(newValue);
-                              if (parsedValue >= 100) {
-                                budgetController.value = TextEditingValue(
-                                  text: newValue,
-                                  selection: TextSelection.collapsed(
-                                    offset: newValue.length,
-                                  ),
-                                );
-                              } else {
-                                budgetController.text = '100';
+                              if (newValue.isNotEmpty &&
+                                  int.tryParse(newValue) != null) {
+                                int parsedValue = int.parse(newValue);
+                                if (parsedValue >= 100) {
+                                  budgetController.value = TextEditingValue(
+                                    text: newValue,
+                                    selection: TextSelection.collapsed(
+                                      offset: newValue.length,
+                                    ),
+                                  );
+                                } else {
+                                  budgetController.text = '100';
+                                }
                               }
-                            }
-                          },
-                        ),
-                        Text(
-                          'Minimum budget is ₱100',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
+                            },
                           ),
-                        ),
-                      ],
+                          Text(
+                            'Minimum budget is ₱100',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Wrap(
-                    spacing: 8,
-                    runSpacing: 8,
-                    children: [100, 500, 1000, 5000, 8000, 10000].map((price) {
-                      return GestureDetector(
-                        onTap: () {
-                          budgetController.text = price.toString();
-                        },
-                        child: Card(
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Container(
-                            padding: EdgeInsets.all(12.0),
-                            width: 125,
-                            child: Center(
-                              child: Text(
-                                '₱$price',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.teal,
+                    Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: [100, 500, 1000, 5000, 8000, 10000].map((price) {
+                        return GestureDetector(
+                          onTap: () {
+                            budgetController.text = price.toString();
+                          },
+                          child: Card(
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.all(12.0),
+                              width: 125,
+                              child: Center(
+                                child: Text(
+                                  '₱$price',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.teal,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ],
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Spacer(),
-          ],
+            ],
+          ),
         ),
       ),
     );
